e4830d8553f27743e02226959888cd9ac8b965b0
Eliza Lin <eliza.lin@silkrode.com.tw>
2021-06-25 15:10:26 +0800

    add modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="modal-view">
+    <div class="overlay" @click="closeModal"></div>
+    <div class="modal">
+      <button class="plus-btn" type="button" @click="plus">+ plus</button>
+      <br />
+      <button class="close-btn" type="button" @click="closeModal">close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {};
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close", true);
+    },
+    plus() {
+      this.$emit("plus", true);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.modal-view {
+  .overlay {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: 999;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  .modal {
+    margin: 0 auto;
+    width: 300px;
+    height: 300px;
+    position: relative;
+    z-index: 1000;
+    padding: 30px;
+    background-color: #fff;
+    .close-btn,
+    .plus-btn {
+      margin: 30px auto 0;
+    }
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="showModal">open modal</button>
+    <Modal v-if="modalVisible" @close="closeModal" @plus="plusCount" />
+
+    <div class="margin-top-20">{{ `count: ${count}` }}</div>
   </div>
 </template>
+
+<script>
+import Modal from "@/components/Modal.vue";
+
+export default {
+  name: "About",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      modalVisible: false,
+      count: 0,
+    };
+  },
+  created() {},
+  methods: {
+    showModal() {
+      this.modalVisible = true;
+    },
+    closeModal() {
+      this.modalVisible = false;
+    },
+    plusCount() {
+      this.count = this.count + 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.margin-top-20 {
+  margin-top: 20px;
+}
+</style>
